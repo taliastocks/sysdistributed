ffddd1f2d500dbc0e1c0b94d99185f792ff3de41
Collin RM Stocks <collinstocks@gmail.com>
2014-04-11 17:22:39 -0400

    Change the way remote commands are executed. Clean up some code.

--- bin/utilities.py
+++ bin/utilities.py
@@ -6,7 +6,7 @@ import subprocess
 from subprocess import PIPE
 
 __all__ = ['execute', 'node_usage', 'copy_to_node', 'SYSDISTRIBUTED',
-    'decode_node', 'PIPE', 'test_ssh']
+    'decode_node', 'PIPE', 'test_ssh', 'remote_execute']
 
 SYSDISTRIBUTED = os.environ.get('SYSDISTRIBUTED')
 
@@ -28,7 +28,10 @@ def decode_node (node):
 
 
 def execute (command, *argv,
-    stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr):
+    stdin=None, stdout=None, stderr=None):
+    stdin = sys.stdin if stdin == None else stdin
+    stdout = sys.stdout if stdout == None else stdout
+    stderr = sys.stderr if stderr == None else stderr
     if command in os.listdir(
         os.path.join(SYSDISTRIBUTED, 'bin')):
         command = os.path.join(SYSDISTRIBUTED, 'bin', command)
@@ -38,16 +41,22 @@ def execute (command, *argv,
 
 
 
-def test_ssh (user, host, port):
+def remote_execute (user, host, port, command,
+    stdin=None, stdout=None, stderr=None):
     return execute('ssh',
         '-i', os.path.join(SYSDISTRIBUTED, 'key'),
         '-p', str(port),
         '%s@%s' % (user, host),
         '-o', 'PreferredAuthentications=publickey',
         '-o', 'IdentitiesOnly=yes',
-        'true',
-        stdout=open('/dev/null'), stderr=open('/dev/null')
-    ).wait() == 0
+        command,
+        stdin=stdin, stdout=stdout, stderr=stderr
+    )
+
+
+
+def test_ssh (user, host, port):
+    return remote_execute(user, host, port, 'true').wait() == 0
 
 
 
